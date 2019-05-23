15cd135a66cc3c8c2c15bc0d09ea1eb8d11a09d4
Matevž Morato <mate2567@gmail.com>
2019-05-23 10:22:02 +0200

    Delujoča verzija projekta.

--- VarnostnaRazdalja/build/Arduino Mega/-.d
+++ VarnostnaRazdalja/build/Arduino Mega/-.d
@@ -1,2 +1,2 @@
-.sc12440680949045295519.o: \
- /home/matevz/eclipseProjects/VarnostnaRazdalja/.sc12440680949045295519.cpp
+.sc1659819442894324579.o: \
+ /home/matevz/eclipseProjects/VarnostnaRazdalja/.sc1659819442894324579.cpp
